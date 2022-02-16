3fe82c6bce1c9d693ffc9f6a99809063e1d5a420
Naman Saini <naman.saini_cs20@gla.ac.in>
2022-02-16 19:41:35 +0530

    Create QueueUsingLinkedList.java

---
+++ QueueUsingLinkedList.java
@@ -0,0 +1,65 @@
+
+class Node3{
+    String data;
+    Node3 next;
+    Node3(String data){
+        this.data = data;
+    }
+}
+class QueuebyLinkedList{
+    Node front;
+    Node rear;
+    Node newnode;
+    public void enqueue(String data){
+        Node3 newnode = new Node3(data);
+    }
+    public boolean isEmpty(){
+        if(front == null && rear == null){
+            front = rear = newnode;
+        }
+        else{
+            rear.next = newnode;
+        }
+        return true;
+
+    }
+//    public void queueDisplay()
+//    {
+//        int i;
+//        if (front == rear) {
+//            System.out.println("Queue is Empty\n");
+//            return;
+//        }
+//
+//        // traverse front to rear and print elements
+//        for (i=front;i<rear;i++) {
+//            System.out.printf(" %d , ",queue[i]);
+//        }
+//        return;
+//    }
+
+
+}
+public class TestQueue {
+    public static void main(String[] args) {
+        QueuebyLinkedList obj = new QueuebyLinkedList();
+        String odd = "";
+        String even = "";
+        for(String str: args) {
+
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(0) != 0) {
+                    odd = odd + str.charAt(i);
+                }
+                else {
+                    even = even + str.charAt(i);
+                }
+
+            }
+//            System.out.println(odd+even);
+        }
+        obj.enqueue(odd+even);
+//        System.out.println(obj);
+
+    }
+}
