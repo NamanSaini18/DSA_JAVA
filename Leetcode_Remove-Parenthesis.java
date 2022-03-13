5d209b24a1d9fce6629e1f50c161e3207150d3aa
Naman Saini <naman.saini_cs20@gla.ac.in>
2022-03-13 14:40:57 +0530

    Solution of Leetcode Problem using Stack DS

---
+++ Leetcode_Remove-Parenthesis.java
@@ -0,0 +1,63 @@
+// A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+
+// For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+// A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
+
+// Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+
+// Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
+
+ 
+
+// Example 1:
+
+// Input: s = "(()())(())"
+// Output: "()()()"
+// Explanation: 
+// The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+// After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+// Example 2:
+
+// Input: s = "(()())(())(()(()))"
+// Output: "()()()()(())"
+// Explanation: 
+// The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+// After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+// Example 3:
+
+// Input: s = "()()"
+// Output: ""
+// Explanation: 
+// The input string is "()()", with primitive decomposition "()" + "()".
+// After removing outer parentheses of each part, this is "" + "" = "".
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s[i] is either '(' or ')'.
+// s is a valid parentheses string.
+class Solution {
+    public String removeOuterParentheses(String str) {
+        
+        Stack <String> stack = new Stack<>();
+		String answer = "";
+		for(int i = 0; i < str.length(); i++) {
+			if((str.charAt(i)+"").equals("(")) {
+				if(!stack.empty()) {
+					answer += "(";
+				}
+				stack.push("(");
+
+			} else {
+				if(stack.peek().equals("(")) {
+					stack.pop();
+					if(!stack.empty()) {
+						answer += ")";
+					}
+				}
+			}
+		}
+		return answer;
+	}
+}
