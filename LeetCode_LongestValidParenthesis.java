88e08fcdd366d43497cdf9dbe9b634f8a538cb1f
Naman Saini <naman.saini_cs20@gla.ac.in>
2022-09-10 00:20:01 +0530

    Create LeetCode_LongestValidParenthesis.java

---
+++ LeetCode_LongestValidParenthesis.java
@@ -0,0 +1,49 @@
+/*
+Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+ 
+
+Example 1:
+
+Input: s = "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()".
+Example 2:
+
+Input: s = ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()".
+Example 3:
+
+Input: s = ""
+Output: 0
+ 
+
+Constraints:
+
+0 <= s.length <= 3 * 104
+s[i] is '(', or ')'.
+*/
+class Solution {
+    public int longestValidParentheses(String s) {
+     Stack<Integer>stack = new Stack<>();
+     stack.push(-1);
+     int max = 0;
+     for(int i=0;i<s.length();i++){
+         char ch = s.charAt(i);
+         if(ch == '(')
+         stack.push(i);
+         else{
+             stack.pop();
+             if(stack.empty()){
+                 stack.push(i);
+             }
+             else{
+                 int len=i-stack.peek();
+                 max=Math.max(max,len);
+             }
+         }
+     }
+     return max;   
+    }
+}
