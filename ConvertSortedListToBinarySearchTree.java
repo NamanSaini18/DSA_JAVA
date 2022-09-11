dd8751791b41fe633304b739b2311186249b3f2c
Naman Saini <naman.saini_cs20@gla.ac.in>
2022-09-11 14:28:16 +0530

    Runtime: 1ms, Faster than 90% submissions

---
+++ ConvertSortedListToBinarySearchTree.java
@@ -0,0 +1,84 @@
+/*
+Given the head of a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+ 
+
+Example 1:
+
+
+Input: head = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: One possible answer is [0,-3,9,-10,null,5], which represents the shown height balanced BST.
+Example 2:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in head is in the range [0, 2 * 104].
+-105 <= Node.val <= 105
+Accepted
+398,381
+Submissions
+699,704
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head == null)
+        {
+            return null;
+        }
+        ListNode temp = null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null)
+        {
+            temp = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        TreeNode newNode = new TreeNode(slow.val);
+        
+            if(temp == null)
+            {
+                return newNode;
+            }
+            temp.next = null;
+            
+            newNode.left = sortedListToBST(head);
+            newNode.right = sortedListToBST(slow.next);
+            
+            return newNode;
+        
+    }
+}
