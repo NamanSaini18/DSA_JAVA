cd5743f108ff04703d40bf7b65754bd156713f42
Naman Saini <naman.saini_cs20@gla.ac.in>
2022-12-01 21:55:59 +0530

    Simple String used
    Runtime: 6 ms, faster than 70.18% of Java online submissions for determining if String Halves Are Alike.
    Memory Usage: 42.4 MB, less than 50.82% of Java online submissions for Determine if String Halves Are Alike.

---
+++ LC_Determine_If_String_Halves_Are_Alike.java
@@ -0,0 +1,57 @@
+/*
+You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
+
+Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
+
+Return true if a and b are alike. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: s = "book"
+Output: true
+Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel. Therefore, they are alike.
+Example 2:
+
+Input: s = "textbook"
+Output: false
+Explanation: a = "text" and b = "book". a has 1 vowel whereas b has 2. Therefore, they are not alike.
+Notice that the vowel o is counted twice.
+ 
+
+Constraints:
+
+2 <= s.length <= 1000
+s.length is even.
+s consists of uppercase and lowercase letters.
+*/
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        int ctr1 = 0;
+        int ctr2 = 0;
+        int i = 0, j = s.length()-1;
+        while(i<j)
+        {
+            if(isVowel(s.charAt(i)))
+            {
+                ctr1++;
+            }
+            if(isVowel(s.charAt(j)))
+            {
+                ctr2++;
+            }
+            i++;
+            j--;
+        }
+        return ctr1 == ctr2;
+    }
+    public boolean isVowel(char str)
+    {
+        if(str=='A' || str=='a'|| str=='E' || str=='e' || str=='I' || str=='i' || str=='O' || str=='o' || str=='U' || str=='u')
+        {
+            return true;
+        }
+        return false;
+    }
+}
